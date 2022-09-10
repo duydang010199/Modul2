c1f8aa0ef7baa6fe6c5ef862653a00c64eb34967
yud <phuocduydang010199@gmail.com>
2022-09-10 22:49:06 +0700

    baitap1

---
+++ baitap1/src/Showtime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Showtime {
+        public static void main(String[] args) {
+            Date now = new Date();
+            System.out.println("Now is: " + now);
+        }
+    }
+
